b9ecd6ccc83370220865e9fa4c6b73589e891b97
RuthHub <Ruth.lin@bcbsnc.com>
2017-04-19 10:26:37 -0400

    Added Welcom message

--- VerySimpleTaskList/Program.cs
+++ VerySimpleTaskList/Program.cs
@@ -1,9 +1,12 @@
-﻿namespace VerySimpleTaskList
+﻿using System;
+
+namespace VerySimpleTaskList
 {
     class Program
     {
         static void Main(string[] args)
         {
+         Console.WriteLine("Welcome to VerySimpleTaskList version 1.0");
             TaskManager manager = new TaskManager();
             manager.Run();
         }
